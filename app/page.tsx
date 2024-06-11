2f733f1dc26ab240e5ffef396e9bd91d5a84527d
nimbledy <nimbledy@gmail.com>
2024-06-11 18:19:57 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,11 @@ import { NEXT_PUBLIC_URL } from './config';
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      label: 'Story time',
-    },
-    {
-      action: 'tx',
-      label: 'Send Base Sepolia',
-      target: `${NEXT_PUBLIC_URL}/api/tx`,
-      postUrl: `${NEXT_PUBLIC_URL}/api/tx-success`,
-    },
+      label: 'Im Nikita!',
+    }
   ],
   image: {
-    src: `${NEXT_PUBLIC_URL}/park-3.png`,
+    src: `${NEXT_PUBLIC_URL}/park-5.jpg`,
     aspectRatio: '1:1',
   },
   input: {
@@ -30,7 +24,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'zizzamia.xyz',
     description: 'LFG',
-    images: [`${NEXT_PUBLIC_URL}/park-1.png`],
+    images: [`${NEXT_PUBLIC_URL}/park-5.jpg`],
   },
   other: {
     ...frameMetadata,
